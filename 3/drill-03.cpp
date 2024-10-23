53f17e60793563146f1a7b11c90f9134fb21dfab
a lice <alicehiqwert@gmail.com>
2024-10-22 20:46:41 -0700

    second part of drill done

--- 3/drill-03.cpp
+++ 3/drill-03.cpp
@@ -7,9 +7,9 @@ int num1, num2;
 int main(){
         cout << "please enter two numbers:\n";
        while(cin >> num1 >>num2){
-        cout << num1 <<" " << num2 << "\n";
-        cout << "please enter two numbers:\n";
-        
+        cout << "the smaller number is " << min(num1, num2) << "\n";
+        cout << "the larger number is " << max(num1, num2) << "\n";
+       cout << "please enter two numbers:\n"; 
        }
 }
         
