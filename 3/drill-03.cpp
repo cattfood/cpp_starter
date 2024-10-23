b42022a25b0c1717f9bc66e4329411f03ff1728f
a lice <alicehiqwert@gmail.com>
2024-10-22 20:50:53 -0700

    third part of drill done

--- 3/drill-03.cpp
+++ 3/drill-03.cpp
@@ -7,9 +7,14 @@ int num1, num2;
 int main(){
         cout << "please enter two numbers:\n";
        while(cin >> num1 >>num2){
+           if (num1 == num2) {
+               cout << "these numbers are equal!!\n";
+           }
+           else {
         cout << "the smaller number is " << min(num1, num2) << "\n";
         cout << "the larger number is " << max(num1, num2) << "\n";
-       cout << "please enter two numbers:\n"; 
+        cout << "please enter two numbers:\n"; 
+           }
        }
 }
         
