4200acb1c0e59f172f8929d03e6808d1a4a8229b
a lice <alicehiqwert@gmail.com>
2024-10-22 20:39:13 -0700

    finished first part of drill

---
+++ 3/drill-03.cpp
@@ -0,0 +1,18 @@
+#include "../PPPheaders.h"
+#include <iostream>
+#include <string>
+
+int num1, num2;
+
+int main(){
+        cout << "please enter two numbers:\n";
+       while(cin >> num1 >>num2){
+        cout << num1 <<" " << num2 << "\n";
+        cout << "please enter two numbers:\n";
+        
+       }
+}
+        
+
+       
+
