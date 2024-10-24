e9e1a53933b9e3f3976cdb73277e4474a173ccbd
a lice <alicehiqwert@gmail.com>
2024-10-23 21:22:33 -0700

    did part 5 of drill

--- 3/drill-03.cpp
+++ 3/drill-03.cpp
@@ -3,18 +3,23 @@
 #include <string>
 
 double num1, num2;
+double similarity = 0.01;
 
 int main(){
         cout << "please enter two numbers:\n";
-       while(cin >> num1 >>num2){
+       while(cin >> num1 >> num2){
            if (num1 == num2) {
                cout << "these numbers are equal!!\n";
            }
            else {
         cout << "the smaller number is " << min(num1, num2) << "\n";
         cout << "the larger number is " << max(num1, num2) << "\n";
-        cout << "please enter two numbers:\n"; 
+        if (fabs(num1 - num2) <= similarity) {
+            cout << "these numbers are almost equal!!\n";
+        } 
            }
+        cout << "please enter two numbers:\n";
+           
        }
 }
         
