57d9eaa4b67455bffb9b8bcc72656938878c6ca9
a lice <alicehiqwert@gmail.com>
2024-11-03 15:44:48 -0800

    finished drill for chapter 3

--- 3/drill-03.cpp
+++ 3/drill-03.cpp
@@ -1,26 +1,40 @@
 #include "../PPPheaders.h"
 #include <iostream>
 #include <string>
+#include <algorithm>
 
-double num1, num2;
-double similarity = 0.01;
+double num1, maxNum, minNum;
+vector<double> nums;
+int amount; //amount of numbers entered
+int total = 0; //total sum of numbers
+
 
 int main(){
-        cout << "please enter two numbers:\n";
-       while(cin >> num1 >> num2){
-           if (num1 == num2) {
-               cout << "these numbers are equal!!\n";
+        cout << "please enter a number (in meters):\n";
+       while(cin >> num1){
+          nums.push_back(num1);
+          total+= num1;
+       if (num1 < minNum) {
+           cout << num1 << " is the smallest number so far!\n";
+           minNum = num1;
+           } 
+       if (num1 > maxNum) {
+           cout << num1 << " is the biggest number so far!\n";
+           maxNum = num1;
            }
-           else {
-        cout << "the smaller number is " << min(num1, num2) << "\n";
-        cout << "the larger number is " << max(num1, num2) << "\n";
-        if (fabs(num1 - num2) <= similarity) {
-            cout << "these numbers are almost equal!!\n";
+           amount++;
+        cout << "please enter a number:\n";
         } 
-           }
-        cout << "please enter two numbers:\n";
-           
-       }
+        cout << "you entered " << amount << " numbers\n";
+        cout << "the biggest number was: " << maxNum << "\n";
+        cout << "the smallest number was: " << minNum <<"\n";
+        cout << "the sum of your numbers is: " << total <<" meters\n";
+        sort(nums.begin(), nums.end());
+        for(int i=0; i < nums.size(); i++) {
+            double numF = nums.at(i);
+            cout << numF << " "; 
+        }
+        cout << "\n";
 }
         
 
